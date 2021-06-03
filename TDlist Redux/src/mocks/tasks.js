c7aeff512b9d640474fa01f13406efae0cf46966
LongHB-web <longhb.bi9147@st.usth.edu.vn>
2021-06-03 10:58:02 +0700

    Todolist with redux

---
+++ TDlist Redux/src/mocks/tasks.js
@@ -0,0 +1,30 @@
+const uuidv4 = require('uuid/v4');
+
+let items = [
+    {
+        id      : uuidv4(),
+        name    : "Abc Lorem ipsum sit amet, consectetur adipisicing elit.",
+        level   : 0 // 2 High, 1 Medium, 0 Small
+    },
+    {
+        id      : uuidv4(),
+        name    : "Def Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        level   : 1 // 2 High, 1 Medium, 0 Small
+    },
+    },
+    {
+        id      : uuidv4(),
+        name    : "Ghj Lorem ipsum sit amet, consectetur adipisicing elit.",
+        level   : 2 // 2 High, 1 Medium, 0 Small
+    },
+    }
+    ,
+    {
+        id      : uuidv4(),
+        name    : "123 Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        level   : 0 // 2 High, 1 Medium, 0 Small
+    },
+    }
+];
+
+export default items;
